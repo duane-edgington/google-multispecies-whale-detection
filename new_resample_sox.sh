bf91034f30abd4b5a4f16015c46add0e25ee9af5
duane <duane@mbari.org>
2025-08-14 16:53:27 -0700

    new script for 24kHz resample

---
+++ new_resample_sox.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# This script runs `sox` for resampling all days in a given year/month,
+# which are required arguments. Example:
+#    ./resample_sox.sh 2018 11
+# Each resample is launched in its own process.
+
+set -ue
+
+year=$1
+month=$2
+days=$(seq 1 31)  # for convenience 1–31 regardless of month
+
+audio_base_dir="/mnt/PAM_Archive"
+
+decimated_base_dir="/mnt/PAM_Analysis/GoogleOrcaModel/resampled_24kHz"
+#decimated_base_dir="/home/duane/google-multispecies-whale-detection/local/PAM_Analysis/GoogleOrcaModel/resampled_24kHz"
+
+days_line="$(echo "${days}" | tr '\n' ' ')"
+
+in_dir=$(printf "%s/%04d/%02d" ${audio_base_dir} "${year}" "${month}")
+
+out_dir=$(printf "%s/%04d/%02d" ${decimated_base_dir} "${year}" "${month}")
+mkdir -p "${out_dir}"
+
+printf "Starting resample_sox.sh: %04d-%02d days: %s\n" "${year}" "${month}" "${days_line}"
+
+#use SoX to decimate/resample the audio data directly. the -v flag is for very high quality.
+#  highpass 10Hz
+# fade logarithmic 0.5 sec fade in, 0 sec hold (i.e. full duration), 0.5 sec fade out
+
+for day in ${days}; do
+  prefix=$(printf "%s/MARS_%04d%02d%02d" "${in_dir}" "${year}" "${month}" "${day}")
+  #for infile in "${prefix}"_23*.wav; do
+  for infile in "${prefix}".wav; do
+    basename=$(basename "${infile}" .wav)
+    outfile="${out_dir}/${basename}_resampled_24kHz.wav"
+    echo "infile = ${infile}"
+    echo "outfile = ${outfile}"
+   sox "${infile}" "${outfile}" rate -v 24000 highpass 10 fade 0.5 0.5  &
+  done
+
+done
+wait
