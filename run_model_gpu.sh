0288b3cd65517b9909460044616225dd1aa54d5b
duane-edgington <duane@mbari.org>
2025-09-18 05:35:51 -0700

    sample script run_model gpu optimized

--- run_model_gpu.sh
+++ run_model_gpu.sh
@@ -1,5 +1,7 @@
-nohup python run_model_gpu.py \
+nohup python run_model_gpu_optimized.py \
 	--input_dir /mnt/PAM_Analysis/GoogleMultiSpeciesWhaleModel2/resampled_24kHz_chunks/2018/04/ \
-	--output_dir /mnt/PAM_Analysis/GoogleMultiSpeciesWhaleModel2/scores/2018/04/ \
-	--model_url "https://www.kaggle.com/models/google/multispecies-whale/TensorFlow2/default/2" > logs/nohup_run_model_2018_04_gpu.out &
+	--output_dir /mnt/PAM_Analysis/GoogleMultiSpeciesWhaleModel2/scores_gpu/2018/04/ \
+	--model_url "https://www.kaggle.com/models/google/multispecies-whale/TensorFlow2/default/2" \
+	--batch_size 8  # Adjust based on your GPU memory
+> logs/nohup_run_model_2018_04_gpu_optimized.out &
 
